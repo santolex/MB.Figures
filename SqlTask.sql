cd88e7c9be1e5b5da2045b95f5ef473c547d3453
santolex <6786981+santolex@users.noreply.github.com>
2019-05-27 10:39:48 +0300

    Fixed sql query.

--- SqlTask.sql
+++ SqlTask.sql
@@ -1,3 +1,18 @@
+Tables: 
+ 1. Products
+ 2. Categories
+ 3. ProductCategories
+
 SELECT p.Name ProductName ,c.Name CategoryName
 FROM Products p
-    LEFT JOIN Categories c ON c.Id=p.CategoryId
+    LEFT JOIN ProductCategories pc ON pc.ProductId=p.Id
+    LEFT JOIN Categories c ON pc.CategoryId=c.id
+
+или так
+
+SELECT p.Name ProductName ,c.Name CategoryName
+FROM Products p
+    (SELECT p.ProductId,c.CategoryName
+     FROM ProductCategories pc 
+        INNER JOIN Categories c ON pc.CategoryId=c.id) a
+    LEFT JOIN ON a.ProductId=p.id
